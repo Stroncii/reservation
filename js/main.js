df59dfcd5ca93951560e2ce1b1f3d637f08af228
Stroncii <anton.stroncii@gmail.com>
2014-02-20 14:11:19 +0200

    Showing/hiding block on checkbox click was realized. Datepick was realized.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@ $(document).ready(function(){
 
 		$('#repeatin').hide();
 		$('#table').hide();
-		
+
+
+
+
+
+		$('#checkBox').click(function() {
+       	 if (!$(this).is(':checked')) {
+          	  $('#repeatin').hide(); 
+         	   } else{
+            	$('#repeatin').show();
+          	  }
+       	});
+
+       	 var date;
+       	 $(document).on('change','#datepicker',function(){
+       	 	date = $(this).datepicker({ dateFormat: 'dd-mm-yy' }).val();
+       	 	alert ("Сегодня " + date);
+       	 });
+
 });
