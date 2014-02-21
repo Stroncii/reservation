6d1123c25f42b2259408f3da6f7b3f770b82278a
Stroncii <anton.stroncii@gmail.com>
2014-02-21 16:18:40 +0200

    Authorization forms were added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,13 @@ $(document).ready(function(){
 		$('#table').hide();
 		var dateObject = $("#datepicker").datepicker("getDate");
 		var dateString = $.datepicker.formatDate("dd-mm-yy", dateObject);
+    var clients = new Array ();
+
+    var Client;
+    function username(name, password) { 
+      this.name = name;
+      this.password = password;
+   }
 
 
 
@@ -22,5 +29,20 @@ $(document).ready(function(){
        	 	date = $(datepicker).datepicker({ dateFormat: 'dd-mm-yy' }).val();
        	 	console.log(date);
        	 });
+
+         $('#button').click(function() {
+          alert ('HOLA');
+        });
+
+         $('#logButton').click(function() {
+          if ($('#username').val() && $('#password').val()){
+            Client = new username($('#username').val(), $('#password').val())
+            clients.push(Client);
+            alert ('Your name = ' + clients[clients.length].username);
+          } else {
+            alert ('Введите логин и пароль');
+          }
+
+        });
 
 });
