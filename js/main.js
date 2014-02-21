7e883b1bd31c4ac9d091640bd4c3ea4737a00ecc
Stroncii <anton.stroncii@gmail.com>
2014-02-21 17:14:19 +0200

    Authorization

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+    var Client;
+    function username(name, password) { 
+      this.name = name;
+      this.password = password;
+    }
+
 $(document).ready(function(){
 
 		$('#repeatin').hide();
 		$('#table').hide();
 		var dateObject = $("#datepicker").datepicker("getDate");
 		var dateString = $.datepicker.formatDate("dd-mm-yy", dateObject);
-    var clients = new Array ();
+    var clients = new Array();
 
-    var Client;
-    function username(name, password) { 
-      this.name = name;
-      this.password = password;
-   }
+    
 
 
 
@@ -36,13 +38,13 @@ $(document).ready(function(){
 
          $('#logButton').click(function() {
           if ($('#username').val() && $('#password').val()){
-            Client = new username($('#username').val(), $('#password').val())
+            Client = new username($('#username').val(), $('#password').val());
             clients.push(Client);
-            alert ('Your name = ' + clients[clients.length].username);
+            alert ('Здравствуйте' + clients[clients.length-1].name + ' ' + clients[clients.length-1].password);
+            $('#table').show();
           } else {
             alert ('Введите логин и пароль');
-          }
+          };
 
         });
-
 });
